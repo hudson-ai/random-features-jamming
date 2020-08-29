b7a199b7ee85058878b5cb45caa8eff47dec5572
Hudson Cooper <hudson@avlab.ai>
2020-08-29 11:37:33 +0000

    Added analysis for random features in addition to the analysis for trained features already found in postprocessing_different_lambdas

--- code/progressive_training/L=1/postprocessing_different_lambdas.py
+++ code/progressive_training/L=1/postprocessing_different_lambdas.py
@@ -45,48 +45,47 @@ for model_name in tqdm.tqdm(model_names):
 
     full_model = tf.keras.models.load_model(model_path+'/MODEL')
     weight_names = [p[:p.find('.index')] for p in os.listdir(model_path) if p.endswith('.ckpt.index')]
-    weight_names = sorted(weight_names, key = lambda s:int(s.split('_')[1].split('.')[0]))
+    weight_names = np.array(sorted(weight_names, key = lambda s:int(s.split('_')[1].split('.')[0])))
 
-#     for weight_name in tqdm.tqdm(weight_names[-1:], leave=False):
-    weight_name = weight_names[-1]
-    for lamb in lambs:
-        batches_per_epoch = 5
-        step = int(weight_name.split('.')[0].split('_')[1])*batches_per_epoch
+    for weight_name in tqdm.tqdm(weight_names[[0, -1]], leave=False):
+        for lamb in lambs:
+            batches_per_epoch = 5
+            step = int(weight_name.split('.')[0].split('_')[1])*batches_per_epoch
 
-        weight_path = os.path.join(model_path, weight_name)
-        full_model.load_weights(weight_path).expect_partial()
+            weight_path = os.path.join(model_path, weight_name)
+            full_model.load_weights(weight_path).expect_partial()
 
-        intermed_model = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(d, name='inputs'),
-            full_model.get_layer('intermediate')
-        ])
+            intermed_model = tf.keras.Sequential([
+                tf.keras.layers.InputLayer(d, name='inputs'),
+                full_model.get_layer('intermediate')
+            ])
 
-        train_features = intermed_model(X_train)
-        test_features = intermed_model(X_test)
+            train_features = intermed_model(X_train)
+            test_features = intermed_model(X_test)
 
-        # Liblinear loss = .5*sum_{i=1}^N[w_i^2] + C*sum_{j=1}^P[y_j - sum_{i=1}^N w_i*x_ji]
-        # Mei et al loss = (N*lam/d)*sum_{i=1}^N[w_i^2] + (1/P)*sum_{j=1}^P[y_j - sum_{i=1}^N w_i*x_ji]
-        C = d/(2*N*P*lamb)
-#             print(C)
+            # Liblinear loss = .5*sum_{i=1}^N[w_i^2] + C*sum_{j=1}^P[y_j - sum_{i=1}^N w_i*x_ji]
+            # Mei et al loss = (N*lam/d)*sum_{i=1}^N[w_i^2] + (1/P)*sum_{j=1}^P[y_j - sum_{i=1}^N w_i*x_ji]
+            C = d/(2*N*P*lamb)
+    #             print(C)
 
-        svm = SVM(penalty='l2', loss='squared_hinge', dual=False, fit_intercept=False, C=C)
-        svm = svm.fit(train_features, y_train)
+            svm = SVM(penalty='l2', loss='squared_hinge', dual=False, fit_intercept=False, C=C)
+            svm = svm.fit(train_features, y_train)
 
-        y_train_hat = svm.decision_function(train_features)
-        y_test_hat = svm.decision_function(test_features)
+            y_train_hat = svm.decision_function(train_features)
+            y_test_hat = svm.decision_function(test_features)
 
-        result = {
-            "P": P,
-            "N": N,
-            "d": d,
-            "step": step,
-            "lambda": lamb,
-            "C": C,
-            "y_train_hat": y_train_hat,
-            "y_test_hat": y_test_hat
+            result = {
+                "P": P,
+                "N": N,
+                "d": d,
+                "step": step,
+                "lambda": lamb,
+                "C": C,
+                "y_train_hat": y_train_hat,
+                "y_test_hat": y_test_hat
 
-        }
-        results.append(result)
-        
+            }
+            results.append(result)
+            
 pickle.dump(results, open('results_different_lambdas.pkl', 'wb'))
 
